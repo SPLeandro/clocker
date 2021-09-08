776bf23fb07d1c4d36fc7f4f08a84b4f5b1ce58c
unknown <lsaantos@outlook.com>
2021-09-08 12:06:18 -0300

    feat: add times available on agenda

--- pages/api/agenda.js
+++ pages/api/agenda.js
@@ -1,7 +1,22 @@
+import {differenceInHours, format, addHours } from 'date-fns';
 import { firebaseServer } from '../../config/firebase/server';
 
 const db = firebaseServer.firestore();
 const agenda = db.collection('agenda');
+
+const startAt = new Date(2021, 1, 1, 8, 0);
+const endAt = new Date(2021, 1, 1, 17, 0);
+const timesBlockList = [];
+
+const totalHours = differenceInHours(endAt, startAt);
+
+for (let blockIndex = 0; blockIndex <= totalHours; blockIndex++){
+    const time = format(addHours(startAt, blockIndex), 'HH:mm');
+    timesBlockList.push({
+        name: 'Disponível',
+        time
+    });
+} 
 
 export default async (req, res) => {
     const [, token] = req.headers.authorization.split(' ');
@@ -18,8 +33,15 @@ export default async (req, res) => {
         .get();
 
         const docs = snapshot.docs.map(doc => doc.data());
+        const usedTimes = docs.map(doc => doc.time);
+        const availableTimes = timesBlockList.filter(block => !usedTimes.includes(block.time));
+        
+        const data = [
+            ...docs,
+            ...availableTimes
+        ]
 
-        return res.status(200).json(docs);
+        return res.status(200).json(data);
     } catch (error){
         console.log(error);
         return res.status(401);
